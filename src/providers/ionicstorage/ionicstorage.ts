e97424052d18321230d36cb6f75d36bb6e61d4f7
marcialwushu <marcialwushu@gmail.com>
2018-08-07 13:28:13 -0300

    ionicstorage provider functions added - set, get, remover and clearAll

--- src/providers/ionicstorage/ionicstorage.ts
+++ src/providers/ionicstorage/ionicstorage.ts
@@ -1,17 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
-/*
-  Generated class for the IonicstorageProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class IonicstorageProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello IonicstorageProvider Provider');
+  constructor(public http: HttpClient, private _storage: Storage) {
+
+  }
+  set(chave: string, valor: string){
+    this._storage.set(chave,valor);
+  }
+
+  remover(chave: string) {
+    this._storage.remove(chave);
+  }
+
+  get(chave: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this._storage.get(chave).then((val) => {
+        resolve(val);
+      }).catch(() => {
+        reject(reject)
+      });
+    });
+  }
+
+  clearAll(){
+    this._storage.clear();
   }
 
 }
