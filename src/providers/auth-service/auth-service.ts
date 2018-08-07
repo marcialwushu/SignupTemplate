68628d7bfec9245938fda0aa2c4712abe8dd3129
marcialwushu <marcialwushu@gmail.com>
2018-08-07 14:51:51 -0300

    auth-service provider setarUsuario, get, registro, login, estarLogado and sair functions createds

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { URL_API } from '../../app/app.api';
 import { Usuario } from '../../model/usuario.model';
 import { IonicstorageProvider } from '../ionicstorage/ionicstorage';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
@@ -22,6 +23,33 @@ export class AuthServiceProvider {
     console.log(valor.result.access_token);
     this._storage.clearAll();
     this._storage.set('usuario', JSON.stringify(this.usuario));
+  }
+
+  //Instaciar usuario na tela de Login
+  setarUsuario(valor: Usuario) {
+    this.usuario = { nome: valor.nome, token: valor.token, email: valor.email }
+    this._storage.set('usuario', JSON.stringify(valor));
+  }
+
+  get(){
+    return this._storage.get('usuario');
+  }
+
+  registro(data): Observable<any> {
+    return this.http.post<any>(this.url + 'registro', JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } });
+  }
+
+  login(data): Observable<any> {
+    return this.http.post<any>(this.url + 'login', JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } });
+  }
+
+  estaLogado(): boolean {
+    return this.usuario != undefined;
+  }
+
+  sair() {
+    this._storage.clearAll();
+    this.usuario = undefined;
   }
 
 }
