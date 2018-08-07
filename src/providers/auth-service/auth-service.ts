41e069a5698adfdb7b0fff1a6064bad455008288
marcialwushu <marcialwushu@gmail.com>
2018-08-07 14:21:56 -0300

    auth-service provider criarUsuario function created

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -2,13 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { URL_API } from '../../app/app.api';
 import { Usuario } from '../../model/usuario.model';
+import { IonicstorageProvider } from '../ionicstorage/ionicstorage';
 
 
 @Injectable()
 export class AuthServiceProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AuthServiceProvider Provider');
+  usuario: Usuario;
+  url: string = URL_API + 'conta/';
+  logado = false;
+
+  constructor(public http: HttpClient, private _storage: IonicstorageProvider) {
+  }
+
+  //Função da Página de Login
+  criarUsuario(valor: any) {
+    this.usuario = { nome: valor.result.user.userName, token: valor.result.access_token, email: valor.result.user.userName }
+    console.log('TOKEN RECEBIDO');
+    console.log(valor.result.access_token);
+    this._storage.clearAll();
+    this._storage.set('usuario', JSON.stringify(this.usuario));
   }
 
 }
