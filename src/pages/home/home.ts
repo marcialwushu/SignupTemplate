d7c9db99f1caedd38ce6f37d3a3f5aa5e5daacf2
marcialwushu <marcialwushu@gmail.com>
2018-08-08 17:24:09 -0300

    getPages() function added home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { SignupPage } from '../signup/signup';
+import { RestProvider } from '../../providers/rest/rest';
+import { Http } from '@angular/http';
+
 
 @Component({
   selector: 'page-home',
@@ -8,12 +11,31 @@ import { SignupPage } from '../signup/signup';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public pages = new Array<any>();
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public restProvider: RestProvider,
+    public http: Http
+
+  ) {
+
+    this.getPages();
 
   }
 
   onSignup(): void{
     this.navCtrl.push(SignupPage)
+  }
+
+  getPages(){
+    this.restProvider.getPage().subscribe
+    (data => {
+      console.log(data);
+      const response = (data as any);
+      this.pages = this.pages.concat(response.results);
+    })
   }
 
 }
